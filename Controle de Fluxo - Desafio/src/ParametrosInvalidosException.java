45af0b9d4189f4583008a43877d981e5f1d0e8c9
Breno Torquato <164020397+Br3nOT@users.noreply.github.com>
2024-05-09 17:22:51 -0300

    Delete Controle de Fluxo - Desafio directory

--- Controle de Fluxo - Desafio/src/ParametrosInvalidosException.java
+++
@@ -1,6 +0,0 @@
-public class ParametrosInvalidosException extends Exception{
-    ParametrosInvalidosException(int p1, int p2 ){
-        super("Você digitou " + p1 + " no primeiro parâmetro, e é maior que " +p2 + ", o segundo parâmetro.");
-
-    }
-}
