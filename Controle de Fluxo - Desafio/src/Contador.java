a349ef107db7a914623dc848e4fadf141596fcd2
Breno Torquato <brenolt@hotmail.com>
2024-05-09 16:38:54 -0300

    koto

---
+++ Controle de Fluxo - Desafio/src/Contador.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+
+public class Contador {
+    public static void main(String[] args) throws Exception {
+        Scanner stdin = new Scanner(System.in);
+        System.out.println("Digite o primeiro parâmetro: ");
+        int parametroUm = stdin.nextInt();
+        System.out.println("Digite o segundo parâmetro: ");
+        int parametroDois = stdin.nextInt();
+        int contagem = parametroDois - parametroUm;
+
+        try {
+			contar(parametroUm, parametroDois);
+
+		}catch (ParametrosInvalidosException e) {
+            System.err.println("O segundo parâmetro deve ser maior que o primeiro." + e) ;
+            System.out.println("FAREI UM AJUSTE E IREI IMPRIMIR DA MESMA FORMA."); 
+
+            contagem = parametroUm - parametroDois;
+            for (int i = 1; i <= contagem; i++) {
+            System.out.println("Imprimindo o número " + i);            
+            }
+		}
+		
+        stdin.close();
+	}
+    
+	static void contar(int parametroUm, int parametroDois ) throws ParametrosInvalidosException {
+        int contagem = parametroDois - parametroUm;
+         
+		
+        if (parametroDois > parametroUm) {
+            
+            contagem = (parametroDois - parametroUm);
+                for (int i = 1; i <= contagem; i++) {
+                System.out.println("Imprimindo o número " + i);            
+            }
+        }
+
+        else if (parametroDois == parametroUm) {
+
+            System.out.println("POR FAVOR DIGITE NÚMEROS DIFERENTES ENTRE SI, MDS.");
+        }
+        else 
+        {
+            throw new ParametrosInvalidosException("\nVOCÊ DIGITOU O PRIMEIRO PARÂMETRO MAIOR DO QUE O SEGUNDO");
+        }
+
+	}
+}
--- Controle de Fluxo - Desafio/src/ControleDeFluxo.java
+++
@@ -1,5 +0,0 @@
-public class ControleDeFluxo {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Teste");
-    }
-}
